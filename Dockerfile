9fb455ac9a7c7a5d767c8b4a779f7d041b0ce8de
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-30 23:13:07 +0530

    basic config added

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+# Dockerfile,
+
+FROM openjdk:21
+RUN mkdir/usr/app
+COPY target/Sting.jar /usr/app
+WORKDIR /usr/app
+ENTRYPOINT [ "java","-jar","Sting.jar" ]
--- src/main/java/com/string/StingApplication.java
+++ src/main/java/com/string/StingApplication.java
@@ -8,6 +8,7 @@ public class StingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StingApplication.class, args);
+		System.out.println("\npublic final class String\n");
 	}
 
 }
