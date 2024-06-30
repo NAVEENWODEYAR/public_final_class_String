2b73382205d3a20cd556a55bdce9ce1c64ad6aa3
Naveen <naveenkrishnawodeyar@gmail.com>
2024-06-30 22:52:32 +0530

    initial commit

---
+++ src/main/java/com/string/StingApplication.java
@@ -0,0 +1,13 @@
+package com.string;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/string/StingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.string;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
