ff1ef391fc738ded3660f0a827871fb40dd1ba64
Naveen <naveenkrishnawodeyar@gmail.com>
2024-07-04 19:55:16 +0530

    stashed changes applied

--- src/test/java/com/string/RemoveOtherFromStringTest.java
+++ src/test/java/com/string/RemoveOtherFromStringTest.java
@@ -1,18 +1,20 @@
 package com.string;
-/**
- * @author Naveen K Wodeyar
- * @date 03-Jul-2024
- */
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import com.string.code.RemoveFromString;
 
+/**
+ * @author Naveen K Wodeyar
+ * @date 03-Jul-2024
+ */
 public class RemoveOtherFromStringTest {
 	
 	@Test
 	public void removeDigitsTest() {
+<<<<<<< Updated upstream
 		assertEquals("OneTwo", RemoveFromString.removedigits("One12Two"));
+=======
+		assertEquals("OneTwo", RemoveFromString.removedigits("One12Two"));	
+>>>>>>> Stashed changes
 	}
-	
 }
