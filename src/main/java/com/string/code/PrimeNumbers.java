a2b12ac569cd99a908b4bd167ab22c086478db82
Naveen <naveenkrishnawodeyar@gmail.com>
2024-12-28 19:19:16 +0530

    Prime numbers

--- src/main/java/com/string/code/PrimeNumbers.java
+++ src/main/java/com/string/code/PrimeNumbers.java
@@ -5,30 +5,37 @@
 package com.string.code;
 
 /**
- * 
+ * PrimeNumbers program to print prime numbers up to a specified limit.
  */
 public class PrimeNumbers {
-	    public static void main(String[] args) {
-	        int limit = 100; // You can set any limit here
-	        System.out.println("Prime numbers up to " + limit + ":");
-	        
-	        for (int num = 2; num <= limit; num++) {
-	            if (isPrime(num)) {
-	                System.out.print(num + " ");
-	            }
-	        }
-	    }
+    public static void main(String[] args) {
+        int limit = 100; // You can set any limit here
+        System.out.println("Prime numbers up to " + limit + ":");
 
-	    public static boolean isPrime(int num) {
-	        if (num <= 1) {
-	            return false;
-	        }
-	        for (int i = 2; i <= Math.sqrt(num); i++) {
-	            if (num % i == 0) {
-	                return false;
-	            }
-	        }
-	        return true;
-	    }
-	
+        // Iterate through numbers from 2 to limit and print the prime numbers
+        for (int num = 2; num <= limit; num++) {
+            if (isPrime(num)) {
+                System.out.print(num + " ");
+            }
+        }
+    }
+
+    /**
+     * Checks if a number is prime.
+     * 
+     * @param num The number to check.
+     * @return true if the number is prime, false otherwise.
+     */
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; // Numbers less than or equal to 1 are not prime
+        }
+        // Check divisibility up to the square root of the number
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // If divisible by any number, it's not prime
+            }
+        }
+        return true; // If no divisors found, it's prime
+    }
 }
