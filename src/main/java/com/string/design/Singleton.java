b3d998202221a04dafbd6db69f1fc27b3f1ac3e5
Naveen <naveenkrishnawodeyar@gmail.com>
2025-07-22 19:23:29 +0530

    SingleTon design pattern demo

---
+++ src/main/java/com/string/design/Singleton.java
@@ -0,0 +1,28 @@
+package com.string.design;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 22-Jul-2025
+ */
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void showMessage() {
+        System.out.println("Hello from Singleton!");
+    }
+
+    public static void main(String[] args) {
+        Singleton s = Singleton.getInstance();
+        s.showMessage();
+    }
+}
