03050b991215f8fc48ea7319311053999e6b43ae
Naveen <naveenkrishnawodeyar@gmail.com>
2025-10-17 21:52:49 +0530

    Scanner demo

--- src/main/java/com/string/code/ScannerDemo.java
+++ src/main/java/com/string/code/ScannerDemo.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
  * @date 16-Oct-2025
  */
 
-
 public class ScannerDemo {
     public static void main(String[] args) {
         // Create a Scanner object to read input from keyboard
