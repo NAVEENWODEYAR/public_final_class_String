5bce15d9ab81939b5b389e07609fd1fda3cd7f38
Naveen <naveenkrishnawodeyar@gmail.com>
2025-12-14 21:23:52 +0530

    ,

--- src/main/java/com/string/obj/ObjectCastingDemo.java
+++ src/main/java/com/string/obj/ObjectCastingDemo.java
@@ -6,19 +6,20 @@ package com.string.obj;
  */
 
 public class ObjectCastingDemo {
+	
     public static void main(String[] args) {
 
         // UPCASTING (Child → Parent)
-        Animal a = new Dog();   // implicit casting
-        a.sound();              // calls Dog's sound()
+        Animal animal = new Dog();   // implicit casting
+        animal.sound();              // calls Dog's sound()
 
         // DOWNCASTING (Parent → Child)
-        Dog d = (Dog) a;        // explicit casting
+        Dog d = (Dog) animal;        // explicit casting
         d.sound();
         d.fetch();
 
         // Unsafe downcasting example (will throw ClassCastException)
-        Animal a2 = new Animal();
+        Animal animal2 = new Animal();
         // Dog d2 = (Dog) a2;   // Uncommenting this will cause runtime error
     }
 }
