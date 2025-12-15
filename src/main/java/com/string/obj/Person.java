d7cec8ad1be6ba7d806fdb27a9b020ffad35382b
Naveen <naveenkrishnawodeyar@gmail.com>
2025-12-15 22:34:37 +0530

    Object comparisoin

---
+++ src/main/java/com/string/obj/Person.java
@@ -0,0 +1,18 @@
+package com.string.obj;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 15-Dec-2025
+ */
+
+class Person {
+    String name;
+    int age;
+    String city;
+
+    Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+}
