478257d782a46f8f0ac8dccf1195c1cbb98b6358
Naveen <naveenkrishnawodeyar@gmail.com>
2025-12-27 00:02:22 +0530

    counting vowles

---
+++ src/main/java/com/string/code/CountVowelsInterview.java
@@ -0,0 +1,45 @@
+package com.string.code;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 26-Dec-2025
+ */
+
+public class CountVowelsInterview {
+
+    // Method to count vowels in a string
+    public static int countVowels(String str) {
+        int count = 0;
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Main method with test cases
+    public static void main(String[] args) {
+
+        String[] testCases = {
+            "Hello World",
+            "Java",
+            "AEIOU",
+            "bcdfg",
+            "",
+            "Interview Question"
+        };
+
+        for (int i = 0; i < testCases.length; i++) {
+            System.out.println(
+                "Input: \"" + testCases[i] + "\" -> Vowels: " +
+                countVowels(testCases[i])
+            );
+        }
+    }
+}
