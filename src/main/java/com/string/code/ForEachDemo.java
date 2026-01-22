c51452ef21f70104e7c9eacfca5d6e2fcceecaa5
Naveen <naveenkrishnawodeyar@gmail.com>
2026-01-22 23:04:52 +0530

    forEach demo

---
+++ src/main/java/com/string/code/ForEachDemo.java
@@ -0,0 +1,20 @@
+package com.string.code;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 22-Jan-2026
+ */
+
+public class ForEachDemo {
+    public static void main(String[] args) {
+
+        // Array of integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Using for-each loop
+        System.out.println("Elements in the array:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
