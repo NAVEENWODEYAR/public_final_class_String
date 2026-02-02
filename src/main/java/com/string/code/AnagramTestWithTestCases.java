5b4850ab2415f82da65783f942dee3725fc872c6
Naveen <naveenkrishnawodeyar@gmail.com>
2026-02-02 22:13:33 +0530

    .}

--- src/main/java/com/string/code/AnagramTestWithTestCases.java
+++ src/main/java/com/string/code/AnagramTestWithTestCases.java
@@ -8,17 +8,6 @@ import java.util.*;
 
 class AnagramTestWithTestCases {
 
-	public static void main(String[] args) {
-        AnagramTestWithTestCases solution = new AnagramTestWithTestCases();
-
-        // LeetCode-like test cases
-        System.out.println(solution.isAnagram("anagram", "nagaram")); // true
-        System.out.println(solution.isAnagram("rat", "car"));         // false
-        System.out.println(solution.isAnagram("listen", "silent"));   // true
-        System.out.println(solution.isAnagram("aacc", "ccac"));       // false
-        System.out.println(solution.isAnagram("", ""));               // true
-    }
-	
     // LeetCode-style method
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
