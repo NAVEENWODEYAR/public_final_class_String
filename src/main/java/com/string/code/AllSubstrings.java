b00b427932d9bbabfdfd09bee01c48f138df1da1
Naveen <naveenkrishnawodeyar@gmail.com>
2026-02-13 22:23:54 +0530

    all the substrings of an string

---
+++ src/main/java/com/string/code/AllSubstrings.java
@@ -0,0 +1,19 @@
+package com.string.code;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 13-Feb-2026
+ */
+
+public class AllSubstrings {
+    public static void main(String[] args) {
+        String str = "abc";
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                System.out.println(str.substring(i, j));
+            }
+        }
+    }
+    
+}
