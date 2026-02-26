9e6ae10b11a486770c73d279aafd3b539273986d
Naveen <naveenkrishnawodeyar@gmail.com>
2026-02-26 21:57:45 +0530

    git

---
+++ src/main/java/com/string/code/DefineGit.java
@@ -0,0 +1,14 @@
+package com.string.code;
+
+/**
+ * @author Naveen K Wodeyar
+ * @date 26-Feb-2026
+ */
+
+public class DefineGit {
+    public static void main(String[] args) {
+        System.out.println("Definition of Git:");
+        System.out.println("Git is a distributed version control system used to track changes in source code during software development.");
+        System.out.println("It helps developers collaborate, manage project history, and maintain different versions of code efficiently.");
+    }
+}
